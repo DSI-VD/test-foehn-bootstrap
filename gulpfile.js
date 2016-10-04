6ca4cb830ffbaa0877fb6aef8535c1d25028b299
Cédric Aellen <cedric.aellen@vd.ch>
2016-10-04 14:24:42 +0200

    gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -6,59 +6,71 @@ var rename = require("gulp-rename");
 var sourcemaps = require('gulp-sourcemaps');
 var gulpSequence = require('gulp-sequence');
 var browserSync = require('browser-sync').create();
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('clean', function() {
-    return del('./assets');
+    return del('./dist');
+});
+
+gulp.task('copy-html', function() {
+    return gulp.src('./*.html')
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('copy-bootstrap', function() {
     return gulp.src('./node_modules/foehn-bootstrap/dist/**/*.*')
-        .pipe(gulp.dest('./assets/bootstrap'));
+        .pipe(gulp.dest('./dist/assets/bootstrap'));
 });
 
 gulp.task('scope-bootstrap', function() {
     var processors = [
         scopify('.twbs')
     ];
-    return gulp.src('./assets/bootstrap/**/*.css')
+    return gulp.src('./dist/assets/bootstrap/**/*.css')
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(rename({
             prefix: "scope-"
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./assets/bootstrap'));
+        .pipe(gulp.dest('./dist/assets/bootstrap'));
 });
 
 gulp.task('copy-foehn', function() {
     return gulp.src('./node_modules/foehn/dist/assets/foehn/**/*.*')
-        .pipe(gulp.dest('./assets/foehn'));
+        .pipe(gulp.dest('./dist/assets/foehn'));
 });
 
 gulp.task('scope-foehn', function() {
     var processors = [
         scopify('.foehn')
     ];
-    return gulp.src('./assets/foehn/**/*.css')
+    return gulp.src('./dist/assets/foehn/**/*.css')
         //.pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(rename({
             prefix: "scope-"
         }))
         //.pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./assets/foehn'));
+        .pipe(gulp.dest('./dist/assets/foehn'));
 });
 
-// Static server
-gulp.task('serve', function() {
+// Default
+gulp.task('default', gulpSequence('clean', 'copy-html', ['copy-bootstrap', 'copy-foehn'], ['scope-bootstrap', 'scope-foehn']));
+
+// Serve
+gulp.task('serve', ['default'], function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./dist/"
         }
     });
 
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-//gulp.task('default', ['clean', 'copy-bootstrap', 'scope-bootstrap', 'copy-foehn', 'scope-foehn'], function() {
-gulp.task('default', gulpSequence('clean', ['copy-bootstrap', 'copy-foehn'], ['scope-bootstrap', 'scope-foehn'], 'serve'));
+//Deploy
+gulp.task('deploy', ['default'], function() {
+  return gulp.src('./dist/**/*.*')
+    .pipe(ghPages());
+});
